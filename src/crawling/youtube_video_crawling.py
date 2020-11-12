683632d3af558118e1eb021ac3278ed383cfda1a
Kim-Byung-Woo <kbw1994@naver.com>
2020-11-12 16:28:14 +0900

    유튜브 영상 댓글 정보 크롤링 예외 처리 추가 - 더보기 버튼이 안보일 경우 - skip 버튼이 오버레이 됬을 경우 - 댓글 전처리 삭제 - 파일명 변경(영상 제목으로)

--- src/crawling/youtube_video_crawling.py
+++ src/crawling/youtube_video_crawling.py
@@ -27,46 +27,26 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-from collections import Counter
-from wordcloud import WordCloud
-from konlpy.tag import Twitter
-
-def get_noun(comment_txt):
-    twitter = Twitter()
-    noun = []
-    
-    if len(comment_txt)>0:
-        tw = twitter.pos(comment_txt)
-        for i,j in tw:
-            if j == 'Noun':
-                noun.append(i)
-    return noun
 #%%
-
 # 크롬 드라이버 실행
 path = 'chromedriver.exe'
 driver = webdriver.Chrome(path)
 
 # 크리에이터의 특정 video 페이지를 연다.
-video_url = 'https://www.youtube.com/watch?v=CQA7cdxozHY&list=WL&index=19&t=0s' #  https://www.youtube.com/watch?v=gH_w8dOGAso
+video_url = 'https://www.youtube.com/watch?v=iRn1lIxjox0&ab_channel=%EC%9D%B4%EC%8A%A4%ED%83%80TV' #  https://www.youtube.com/watch?v=gH_w8dOGAso
 driver.get(video_url)
 
 delay = 3
 driver.implicitly_wait(delay)
-#driver.maximize_window()
+driver.maximize_window()
 body = driver.find_element_by_tag_name('body')
 time.sleep(delay)
 
+#%%
+# 영상 댓글 크롤링
+ad_time = 0 # 광고 영상이 나오는 경우를 대비해서 광고 시간 카운팅
 SCROLL_PAUSE_TIME = 0.5 # 한번 스크롤 하고 멈출 시간 설정
-# 더보기 버튼 클릭을 위해 최초 1회 스크롤 다운
-#body.send_keys(Keys.PAGE_DOWN)
-#time.sleep(SCROLL_PAUSE_TIME)
-
-# 더보기 클릭
-more_button = driver.find_element_by_xpath('//*[@id="more"]/yt-formatted-string')
-more_button.click()
-
-# 크롤링을 위해 화면 맨 아래까지 스크롤 내리기
+# 댓글 크롤링을 위해 화면 맨 아래까지 스크롤 내리기
 while True:
     last_height = driver.execute_script('return document.documentElement.scrollHeight')
     # 현재 화면의 길이를 리턴 받아 last_height에 넣음
@@ -74,6 +54,7 @@ while True:
         body.send_keys(Keys.PAGE_DOWN)
         # body 본문에 END키를 입력(스크롤내림)
         time.sleep(SCROLL_PAUSE_TIME)
+        ad_time += SCROLL_PAUSE_TIME
     new_height = driver.execute_script('return document.documentElement.scrollHeight')
     if new_height == last_height:
         break;
@@ -96,19 +77,48 @@ df_comment_info = pd.DataFrame()
 df_comment_info['comment id'] = comment_id
 df_comment_info['comment'] = comment_contents
 
+body.send_keys(Keys.HOME) # 제일 상위 페이지로 복귀 - 영상정보 크롤링 하기 위해서
+'''
+if ad_time < 15.0:
+    print('광고 시간이 남았습니다.')    
+    time.sleep(15.0 - ad_time)
+else:
+    pass
+'''
+#%%
+# 영상 정보 크롤링
+# 더보기 버튼 클릭을 위해 최초 1회 스크롤 다운
+body.send_keys(Keys.PAGE_DOWN)
+time.sleep(SCROLL_PAUSE_TIME)
+# 더보기 클릭
+try:
+    more_button = driver.find_element_by_xpath('//*[@id="more"]/yt-formatted-string')
+    more_button.click()
+    video_desc = soup.find('div', {'id' : 'description'}).text
+except : # 더보기 버튼이 없는 경우(비공개 채널)
+    video_desc = 'Private'
+
+try:
+    skip_button = driver.find_element_by_xpath('//*[@id="skip-button:6"]/span/button')
+    skip_button.click()
+except : # skip 버튼이 없는 경우
+    pass
 # 각각의 태그들로 영상 정보추출을 한다.
 video_title = soup.find('h1', 'title style-scope ytd-video-primary-info-renderer').text
 channel_name = soup.find('ytd-channel-name', {'id' : 'channel-name'}).find('a', 'yt-simple-endpoint style-scope yt-formatted-string').text
 subscribe = soup.find('yt-formatted-string',  {'id' : 'owner-sub-count'}).text
-subscribe_num = (float(''.join(ele for ele in subscribe if ele.isdigit() or ele == '.'))) # 문자열에서 소수점 추출
-if subscribe.find('만') != -1:
-    subscribe_num = subscribe_num * 10000
-elif subscribe.find('천') != -1:
-    subscribe_num = subscribe_num * 1000
-else:
-    subscribe_num = subscribe_num
+if subscribe != '':
+    subscribe_num = (float(''.join(ele for ele in subscribe if ele.isdigit() or ele == '.'))) # 문자열에서 소수점 추출
+    if subscribe.find('만') != -1:
+        subscribe_num = subscribe_num * 10000
+    elif subscribe.find('천') != -1:
+        subscribe_num = subscribe_num * 1000
+    else:
+        subscribe_num = subscribe_num
+else: # 구독자 비공개인 경우
+    subscribe_num = 0
+
 video_length = soup.find('span',  {'class' : 'ytp-time-duration'}).text
-video_desc = soup.find('div', {'id' : 'description'}).text
 upload_date = soup.find('div', {'id' : 'date'}).find('yt-formatted-string', 'style-scope ytd-video-primary-info-renderer').text
 upload_date = upload_date.replace('. ', '-') # 날짜 포맷 수정
 upload_date = upload_date.replace('.', '') # 날짜 포맷 수정
@@ -122,12 +132,12 @@ if like_count != '':
 else:
     like_cnt = 'Private'
     unlike_cnt = 'Private'
-    
+
 comment_cnt = soup.find('ytd-comments', {'id' : 'comments'}).find('yt-formatted-string', 'count-text style-scope ytd-comments-header-renderer').text
 
 driver.close()
-
-# 영상 데이터 프레임 생성
+#%%
+# 영상 정보 xlsx 파일 생성
 df_video_info = pd.DataFrame()
 df_video_info['channel name'] = pd.Series(channel_name)
 df_video_info['subscribe count'] =  pd.Series(subscribe_num)
@@ -140,38 +150,18 @@ df_video_info['unlike count'] =  unlike_cnt
 df_video_info['comment count'] =  pd.Series(int(''.join(ele for ele in comment_cnt if ele.isdigit() or ele == '.'))) # 문자열에서 숫자 추출
 df_video_info['describe'] =  pd.Series(video_desc)
 
+import os
+file_dir = os.getcwd() # 현재 파일 경로 추출
+file_dir = os.path.dirname(file_dir) # 상위 경로 추출
+file_dir = os.path.dirname(file_dir) # 상위 경로 추출 - src 폴더에 하위폴더인 crawling 폴더에 소스코드 있어서 상위 경로 추출을 2번 합니다
 
-
-# 이모티콘 제거
-emoji_pattern = re.compile("["
-                u"\U0001F600-\U0001F64F"  # emoticons
-                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                u'\U00010000-\U0010ffff'  # not BMP characters
-    "]+", flags=re.UNICODE)
 
 # 분석에 어긋나는 불용어구 제외 (특수문자, 의성어)
 han = re.compile(r'[ㄱ-ㅎㅏ-ㅣ!?~,".\n\r#\ufeff\u200d]')
- 
-# 그 다음으로는 기존의 데이터에서 댓글컬럼만 뽑아냅니다
-comment_list = []
-for i in range(len(df_comment_info)):
-    comment_list.append(df_comment_info['comment'].iloc[i])
- 
-
-# 최종적으로 compile한 문자열을 이용하여 불용어구를 제외하고 댓글을 보기 쉽게 데이터 프레임으로 저장합니다.
-comment_result = []
-for i in comment_list:
-    tokens = re.sub(emoji_pattern,"",i)
-    tokens = re.sub(han,"",tokens)
-    comment_result.append(tokens)
-
-df_comment_info['mining comment'] = comment_result
-
+file_name = re.sub(han,"",video_title)
 
 # xlsx 파일 생성
-xlxs_dir = channel_name + '_video_info.xlsx'
+xlxs_dir = file_dir + '/data/' + file_name + '_video_info.xlsx'
 # Write two DataFrames to Excel using to_excel(). Need to specify an ExcelWriter object first.
 # 2개 이상의 DataFrame을 하나의 엑셀 파일에 여러개의 Sheets 로 나누어서 쓰려면 먼저 pd.ExcelWriter() 객체를 지정한 후에, sheet_name 을 나누어서 지정하여 써주어야 합니다. 
 with pd.ExcelWriter(xlxs_dir) as writer:
@@ -179,10 +169,8 @@ with pd.ExcelWriter(xlxs_dir) as writer:
     df_comment_info.to_excel(writer, sheet_name = 'comment')
 #%%
 # load file
-
 df_read_video_info = pd.read_excel(xlxs_dir, sheet_name = 'video')
 df_read_comment = pd.read_excel(xlxs_dir, sheet_name = 'comment')
-
 
 for idx in range(len(df_read_comment)):
     comment = df_read_comment.iloc[idx]['comment']
