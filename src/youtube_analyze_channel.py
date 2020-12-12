f7885799bf32b42ee30fb8fcb1545b96f52b9b6d
Kim-Byung-woo <kbw1994@naver.com>
2020-12-13 02:34:56 +0900

    누적구독자 변화에 따른 조회수 시각화 추가
    일일 업로드 영상 개수별 조회수 시각화 수정
    영상 길이별 조회수 시각화 수정

--- src/youtube_analyze_channel.py
+++ src/youtube_analyze_channel.py
@@ -9,17 +9,22 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 #%%
-# 조회수별 구독자 변화 시각화
-# load file
-xlxs_dir = '오킹TV_channel_info.xlsx'
-df_read_channel_info = pd.read_excel(xlxs_dir)
-df_read_channel_info = df_read_channel_info.iloc[:, 1:] # excel 파일에 에서 가져온 index colum 삭제
+file_dir = os.getcwd() # 현재 파일 경로 추출
+file_dir = os.path.dirname(file_dir) # 상위 경로 추출 - 코드 파일과 단어 리스트 파일 위치가 틀려서
 
-plt.rc('font', size = 20, family='gulim') 
+# 일일 구독자수에 따른 조회수 시각화
+# load file
+xlxs_dir = file_dir + '/data/이스타TV_channel_info.xlsx'
+
+df_read_channel_info = pd.read_excel(xlxs_dir)
+#df_read_channel_info = df_read_channel_info.iloc[:, 1:] # excel 파일에 에서 가져온 index colum 삭제
+
+plt.rc('font', size = 20, family='gulim')
 fig, ax0 = plt.subplots(figsize=(25, 10))
 ax1 = ax0.twinx()
-ax0.set_title("조회수별 구독자 변화")
+ax0.set_title("일일 구독자수에 따른 조회수 변화")
 ax0.plot(df_read_channel_info['daily subscribe count'], 'r-', label= '일일 구독자 변화')
 ax0.set_ylabel("일일 구독자 변화")
 ax0.grid(False)
@@ -30,29 +35,62 @@ ax1.grid(False)
 ax1.legend(loc = 2) # 범주
 plt.show()
 #%%
-# 영상개수별 조회수 시각화
-xlxs_dir = '이스타TV_upload_cnt.xlsx'
+# 누적 구독자수에 따른 조회수 시각화
+xlxs_dir = file_dir + '/data/이스타TV_channel_info.xlsx'
+
 df_read_channel_info = pd.read_excel(xlxs_dir)
 df_read_channel_info = df_read_channel_info.iloc[:, 1:] # excel 파일에 에서 가져온 index colum 삭제
 
-df_read_upload_view = df_read_channel_info.groupby("upload_date").sum() # 날짜별로 영상 조회수 합산
+plt.rc('font', size = 20, family='gulim')
+fig, ax0 = plt.subplots(figsize=(25, 10))
+ax1 = ax0.twinx()
+ax0.set_title("누적 구독자수에 따른 조회수 변화")
+ax0.plot(df_read_channel_info['subscribe count'], 'r-', label= '누적 구독자 변화')
+ax0.set_ylabel("누적 구독자 변화")
+ax0.grid(False)
+ax0.legend(loc = 1) # 범주
+ax1.plot(df_read_channel_info['daily view count'], 'g-', label='일일 조회수')
+ax1.set_ylabel("일일 조회수")
+ax1.grid(False)
+ax1.legend(loc = 2) # 범주
+plt.show()
+#%%
+# 일일 영상 업로드 개수별 조회수 시각화
+# load file
+xlxs_dir = file_dir + '/data/이스타TV_upload_cnt.xlsx'
+
+df_read_channel_info = pd.read_excel(xlxs_dir)
+# df_read_channel_info = df_read_channel_info.iloc[:, 1:] # excel 파일에 에서 가져온 index colum 삭제
+
+df_read_upload_view = df_read_channel_info.groupby("upload date").sum() # 날짜별로 영상 조회수 합산
 df_read_upload_cnt = pd.read_excel(xlxs_dir, sheet_name = 'upload count') # 날짜별 영상 개수 불러오기
 
-df_upload_info = pd.merge(df_read_upload_view, df_read_upload_cnt, on = 'upload_date') # 날짜별로 영상 조회수, 개수 분류
-df_anal = df_upload_info.groupby("daily video count").mean() # 영상 개수별 평균값 추출
-df_anal = df_anal.reset_index()
+df_upload_info = pd.merge(df_read_upload_view, df_read_upload_cnt, on = 'upload date') # 날짜별로 영상 조회수, 개수 분류
+# 영상 개수별 평균값 추출
+df_anal = df_upload_info.groupby("daily video count")['view count'].sum() # 일일 영상 업로드 개수별 조회수 합산
+df_anal = df_anal.reset_index() # seires에서 dataframe으로 타입 변경
+df_anal['video count'] = list(df_upload_info.groupby("daily video count")['upload date'].count()) # 업로드 영상개수 별로 업로드 날짜 카운팅, list로 바인딩 안하면 1행씩 밀림
 
+# 평균 조회수 = 총 조회수 / (일일 업로드 영상 개수 * 업로드 영상 개수별 날짜 개수) 
+# ex. 평균 조회수 = 총 조회수 / (1 * 업로드 영상 개수가 1개인 날짜들의 개수)
+df_anal['average view count'] = df_anal['view count'] / (df_anal['daily video count'] * df_anal['video count']) 
+
+# 시각화
+plt.rc('font', size = 10, family='gulim')
 sns.barplot(
     data= df_anal,
     x = "daily video count",
-    y= "view count")
+    y = "average view count")
+plt.xlabel('일일 영상 업로드 개수')
+plt.ylabel('영상 평균 조회수')
 plt.show()
-
 #%%
-# 영상길이별 조회수 시각화
-xlxs_dir = '오킹tv_upload_cnt.xlsx'
+# 영상 길이별 조회수 시각화
+# load file
+xlxs_dir = file_dir + '/data/이스타TV_upload_cnt.xlsx'
+
 df_read_channel_info = pd.read_excel(xlxs_dir)
-df_read_channel_info = df_read_channel_info.iloc[:, 1:] # excel 파일에 에서 가져온 index colum 삭제
+# df_read_channel_info = df_read_channel_info.iloc[:, 1:] # excel 파일에 에서 가져온 index colum 삭제
 
 # 영살 길이별 라벨링
 list_rt = df_read_channel_info['running time']
@@ -65,19 +103,25 @@ for idx in range(len(list_rt)):
 # 기존 데이터에 라벨링 추가
 df_read_channel_info['rt_label'] = list_rt_label
 
+df_anal = df_read_channel_info.groupby("rt_label")['view count'].sum() # 영살 길이 라벨별 조회수 합산 영살길이별 영상 조회수 평균
+df_anal = df_anal.reset_index() # seires에서 dataframe으로 타입 변경
+df_anal['video count'] = list(df_read_channel_info.groupby("rt_label")['rt_label'].count()) # 영살 길이별 영상 개수
+df_anal = df_anal.drop(df_anal[df_anal['video count'] < 10].index) # 영상 개수가 10개미만 행 삭제
+df_anal['average view count'] = df_anal['view count'] / (df_anal['video count']) # 평균 조회수 = 영상 길이별 조회수 합산 /  영상 길이별 영상 개수
 
-
-df_anal = df_read_channel_info.groupby("rt_label")['view count'].mean() # 영살길이별 영상 조회수 평균
-df_anal = df_anal.reset_index()
-df_anal['rt_label count'] = list(df_read_channel_info.groupby("rt_label")['rt_label'].count()) # 영상 라벨링 별로 개수
-df_anal = df_anal.drop(df_anal[df_anal['rt_label count'] < 2].index) # 영상 개수가 10개미만 라벨 행 삭제
-
-
+# 시각화
+plt.rc('font', size = 10, family='gulim')
 sns.barplot(
     data= df_anal,
     x = "rt_label",
-    y= "view count")
+    y = "average view count")
+plt.xlabel('영상 길이')
+plt.ylabel('영상 평균 조회수')
 plt.show()
+#%%
+
+
+
 
 #%%
 '''
